46e8619840da17bf9ded092bd5db4e8dd2de0873
Jake Nimergood <jakenimergood@wireless-10-145-156-65.public.utexas.edu>
2020-03-02 11:47:38 -0600

    Yay

--- src/components/ControlDescriptionPanel.js
+++ src/components/ControlDescriptionPanel.js
@@ -1,10 +1,104 @@
 import React from 'react';
-import { CreditCard, LocalOffer, Explore, ChildCare, Security, ExitToApp, Lock, Person } from '@material-ui/icons';
+import { Email, CreditCard, LocalOffer, Explore, ChildCare, Security, ExitToApp, Lock, Person } from '@material-ui/icons';
+import DescriptionField from "./DescriptionField";
+import { withStyles, makeStyles } from "@material-ui/core";
+import MuiGridList from '@material-ui/core/GridList'
+
+const GridList = withStyles(theme => ({
+  root: {
+    maxHeight: 500,
+    width: '100%',
+  },
+  content: {
+
+  },
+})) (MuiGridList);
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: 350,
+  },
+  content: {
+
+  },
+}));
 
 export default function ControlDescriptionPanel(props) {
-  return (
-    <div>
 
+  const classes = useStyles();
+
+  const [expanded, setExpanded] = React.useState(false);
+  const handleChange = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <GridList>
+        <DescriptionField
+          name={'Email Security'}
+          icon={<Email />}
+          details={'How well does this website protect your email address?'}
+          expanded={expanded === 'p1'}
+          onChange={handleChange('p1')}
+        />
+        <DescriptionField
+          name={'PII Security'}
+          icon={<CreditCard />}
+          details={'How well does this website protect your credit card information, address, and social security number?'}
+          expanded={expanded === 'p2'}
+          onChange={handleChange('p2')}
+        />
+        <DescriptionField
+          name={'Targeted Advertising'}
+          icon={<LocalOffer />}
+          details={'Does this website use your information for targeted advertisements?'}
+          expanded={expanded === 'p3'}
+          onChange={handleChange('p3')}
+        />
+        <DescriptionField
+          name={'Location Tracking'}
+          icon={<Explore />}
+          details={'How well does this website protect your location information?'}
+          expanded={expanded === 'p4'}
+          onChange={handleChange('p4')}
+        />
+        <DescriptionField
+          name={'COPPA Compliance'}
+          icon={<ChildCare />}
+          details={'How well does this website comply with COPPA?'}
+          expanded={expanded === 'p5'}
+          onChange={handleChange('p5')}
+        />
+        <DescriptionField
+          name={'Law Enforcement'}
+          icon={<Security />}
+          details={'How well does this website protect your information from law enforcement?'}
+          expanded={expanded === 'p6'}
+          onChange={handleChange('p6')}
+        />
+        <DescriptionField
+          name={'Privacy Policy Opt-In/Opt-Out'}
+          icon={<ExitToApp />}
+          details={'Does this website allow you to opt out after changing their privacy policy?'}
+          expanded={expanded === 'p7'}
+          onChange={handleChange('p7')}
+        />
+        <DescriptionField
+          name={'Data Control'}
+          icon={<Lock />}
+          details={'How much control does this website give the user over their data?'}
+          expanded={expanded === 'p8'}
+          onChange={handleChange('p8')}
+        />
+        <DescriptionField
+          name={'PII Stripping'}
+          icon={<Person />}
+          details={'How well does this website strip PII from collected data?'}
+          expanded={expanded === 'p9'}
+          onChange={handleChange('p9')}
+        />
+      </GridList>
     </div>
   );
 }
--- src/components/GDPRDescriptionPanel.js
+++ src/components/GDPRDescriptionPanel.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import DescriptionField from "./DescriptionField";
-import {
-  Send,
-  Map,
-  PhonelinkErase,
-  NotificationImportant,
-  ThumbDown,
-  Face,
-  EnhancedEncryption,
-  ContactSupport,
-  DoneAll,
-  Warning,
-  RemoveCircle
-} from "@material-ui/icons";
-import {withStyles} from "@material-ui/core";
+import { Send, Map, PhonelinkErase, NotificationImportant, RemoveCircle, Face, EnhancedEncryption, ContactSupport, DoneAll, Warning } from "@material-ui/icons";
+import { withStyles, makeStyles } from "@material-ui/core";
 import MuiGridList from '@material-ui/core/GridList'
-import {makeStyles} from "@material-ui/core/styles";
-
 
 const GridList = withStyles(theme => ({
   root: {
-
+    maxHeight: 500,
+    width: '100%',
   },
   content: {
 
@@ -29,7 +16,7 @@ const GridList = withStyles(theme => ({
 
 const useStyles = makeStyles(theme => ({
   root: {
-    maxWidth: 350,
+    width: 350,
   },
   content: {
 
@@ -40,6 +27,11 @@ export default function GDPRDescriptionPanel(props) {
 
   const classes = useStyles();
 
+  const [expanded, setExpanded] = React.useState(false);
+  const handleChange = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
   return (
     <div className={classes.root}>
       <GridList>
@@ -47,42 +39,71 @@ export default function GDPRDescriptionPanel(props) {
           name={'Between Site Transfer'}
           icon={<Send />}
           details={'Does this website share your information with other websites?'}
+          expanded={expanded === 'p1'}
+          onChange={handleChange('p1')}
         />
         <DescriptionField
           name={'Company Location'}
           icon={<Map />}
           details={'Does this website disclose where the company is based and where your information will be processed and or transferred?'}
+          expanded={expanded === 'p2'}
+          onChange={handleChange('p2')}
         />
         <DescriptionField
           name={'Right To Be Forgotten'}
           icon={<PhonelinkErase />}
           details={'Does this website support the right to be forgotten? That is, when requested will the website delete all of the users information?'}
+          expanded={expanded === 'p3'}
+          onChange={handleChange('p3')}
         />
         <DescriptionField
           name={'Data Retention Notification'}
           icon={<NotificationImportant />}
           details={'If this website retains information after requesting to be forgotten, will they inform you?'}
+          expanded={expanded === 'p4'}
+          onChange={handleChange('p4')}
         />
         <DescriptionField
           name={'Reject Usage of PII'}
           icon={<RemoveCircle />}
           details={'Does this website allow the user the ability to reject usage of users PII (Personally Identifiable Information)?'}
+          expanded={expanded === 'p5'}
+          onChange={handleChange('p5')}
         />
         <DescriptionField
           name={'Under 16 Protection'}
           icon={<Face />}
           details={'Does this website collect the data of users under the age of 16?'}
+          expanded={expanded === 'p6'}
+          onChange={handleChange('p6')}
         />
         <DescriptionField
           name={'Data Encryption'}
           icon={<EnhancedEncryption />}
           details={'Does this website encrypt user\'s data?'}
+          expanded={expanded === 'p7'}
+          onChange={handleChange('p7')}
         />
         <DescriptionField
-
+          name={'Data Processing Consent'}
+          icon={<ContactSupport />}
+          details={'Does this website ask for the users consent to perform data processing?'}
+          expanded={expanded === 'p8'}
+          onChange={handleChange('p8')}
         />
         <DescriptionField
-
+          name={'Principles of Data Processing'}
+          icon={<DoneAll />}
+          details={'Does this website meet all the principles of data processing?'}
+          expanded={expanded === 'p9'}
+          onChange={handleChange('p9')}
+        />
+        <DescriptionField
+          name={'Breach Notification'}
+          icon={<Warning />}
+          details={'Does this website notify you of security breaches?'}
+          expanded={expanded === 'p10'}
+          onChange={handleChange('p10')}
         />
       </GridList>
     </div>
