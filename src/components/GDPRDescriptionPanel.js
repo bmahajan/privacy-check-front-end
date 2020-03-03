4ca4a2a4e7128719e0c2b64e6891dd8f7af5f225
Jake Nimergood <JakeNimergood@gmail.com>
2020-03-03 10:41:03 -0600

    Jakes stuff:

--- src/components/GDPRDescriptionPanel.js
+++ src/components/GDPRDescriptionPanel.js
@@ -21,10 +21,13 @@ export default function GDPRDescriptionPanel(props) {
   const classes = useStyles();
 
   const [expanded, setExpanded] = React.useState(false);
+  const [scores, setScores] = React.useState(['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-']);
 
-  const handleChange = panel => (event, isExpanded) => {
+  const handleExpansionChange = panel => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
+
+  setTimeout(() => setScores([100, 0, 0, 0, 0, 0, 0, 0, 0, 0]), 2000);
 
   return (
     <div className={classes.root}>
@@ -34,80 +37,80 @@ export default function GDPRDescriptionPanel(props) {
           icon={<Send />}
           details={'Does this website share your information with other websites?'}
           expanded={expanded === 'p1'}
-          onChange={handleChange('p1')}
-          score={0}
+          onChange={handleExpansionChange('p1')}
+          score={scores[0]}
         />
         <DescriptionField
           name={'Company Location'}
           icon={<Map />}
           details={'Does this website disclose where the company is based and where your information will be processed and or transferred?'}
           expanded={expanded === 'p2'}
-          onChange={handleChange('p2')}
-          score={0}
+          onChange={handleExpansionChange('p2')}
+          score={scores[1]}
         />
         <DescriptionField
           name={'Right To Be Forgotten'}
           icon={<PhonelinkErase />}
           details={'Does this website support the right to be forgotten? That is, when requested will the website delete all of the users information?'}
           expanded={expanded === 'p3'}
-          onChange={handleChange('p3')}
-          score={0}
+          onChange={handleExpansionChange('p3')}
+          score={scores[2]}
         />
         <DescriptionField
           name={'Data Retention Notification'}
           icon={<NotificationImportant />}
           details={'If this website retains information after requesting to be forgotten, will they inform you?'}
           expanded={expanded === 'p4'}
-          onChange={handleChange('p4')}
-          score={0}
+          onChange={handleExpansionChange('p4')}
+          score={scores[3]}
         />
         <DescriptionField
           name={'Reject Usage of PII'}
           icon={<RemoveCircle />}
           details={'Does this website allow the user the ability to reject usage of users PII (Personally Identifiable Information)?'}
           expanded={expanded === 'p5'}
-          onChange={handleChange('p5')}
-          score={0}
+          onChange={handleExpansionChange('p5')}
+          score={scores[4]}
         />
         <DescriptionField
           name={'Under 16 Protection'}
           icon={<Face />}
           details={'Does this website collect the data of users under the age of 16?'}
           expanded={expanded === 'p6'}
-          onChange={handleChange('p6')}
-          score={0}
+          onChange={handleExpansionChange('p6')}
+          score={scores[5]}
         />
         <DescriptionField
           name={'Data Encryption'}
           icon={<EnhancedEncryption />}
           details={'Does this website encrypt user\'s data?'}
           expanded={expanded === 'p7'}
-          onChange={handleChange('p7')}
-          score={0}
+          onChange={handleExpansionChange('p7')}
+          score={scores[6]}
         />
         <DescriptionField
           name={'Data Processing Consent'}
           icon={<ContactSupport />}
           details={'Does this website ask for the users consent to perform data processing?'}
           expanded={expanded === 'p8'}
-          onChange={handleChange('p8')}
-          score={0}
+          onChange={handleExpansionChange('p8')}
+          score={scores[7]}
         />
         <DescriptionField
           name={'Principles of Data Processing'}
           icon={<DoneAll />}
           details={'Does this website meet all the principles of data processing?'}
           expanded={expanded === 'p9'}
-          onChange={handleChange('p9')}
-          score={0}
+          onChange={handleExpansionChange('p9')}
+          score={scores[8]}
         />
         <DescriptionField
           name={'Breach Notification'}
           icon={<Warning />}
           details={'Does this website notify you of security breaches?'}
           expanded={expanded === 'p10'}
-          onChange={handleChange('p10')}
-          score={0}
+          onChange={handleExpansionChange('p10')}
+          score={scores[9]}
         />
       </GridList>
     </div>
