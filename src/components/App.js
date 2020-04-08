638ed768a7f1f4d08e8a9eb9bbb0c2ccd515b555
--global <safaianya@gmail.com>
2020-04-08 15:11:36 -0500

    accepted incoming changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'
 import PanelManager from './PanelManager'
 
@@ -34,41 +33,4 @@ import PanelManager from './PanelManager'
         
       }
       
-=======
-import React, { Component } from 'react'
-import PanelManager from './PanelManager'
-
-    export default class App extends Component {
-
-      state = {
-          items: [],
-          isLoaded: false,
-      }
-    
-      componentDidMount() {
-        fetch('https://n08kagpdqh.execute-api.us-east-2.amazonaws.com/dev/database_get/facebook.com')
-        .then(res => res.json())
-        .then((data) => {
-          this.setState({ 
-              isLoaded: true,
-              items: data,
-         })
-        })
-        .catch(console.log)
-      }
-
-      render() {
-
-        var {isLoaded, items} = this.state;
-        
-        if(!isLoaded){
-            return <div>Loading...</div>;
-        } 
-        else{
-            return(items.Market_Sector);
-        }
-        
-      }
-      
->>>>>>> efc9481ff26d6d801296b9d7fd080cfb3dd1c8f8
     }
