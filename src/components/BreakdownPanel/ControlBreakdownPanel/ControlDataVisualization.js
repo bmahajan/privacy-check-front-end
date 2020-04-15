376b55d00c802db25cc86a896e04355f5c4a9e84
ayushsriv <ayushsriv15@gmail.com>
2020-04-15 12:31:52 -0500

    Fixed Visualization

--- src/components/BreakdownPanel/ControlBreakdownPanel/ControlDataVisualization.js
+++ src/components/BreakdownPanel/ControlBreakdownPanel/ControlDataVisualization.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Box } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import data from '../../../data/sampledata.json'
+import { ApiResponseContext} from "../../PanelManager";
 import { ResponsiveBullet } from '@nivo/bullet'
 
 
@@ -15,15 +15,33 @@ const useStyles = makeStyles(theme => ({
 export default function ControlDataVisualization(props) {
 
   const classes = useStyles();
+  const response = React.useContext(ApiResponseContext);
+
+  let data = [
+    {
+      "id": "Contol Score",
+      "ranges": [
+        40,
+        60,
+        80,
+        90,
+        100
+      ],
+      "measures": [
+        response.Control_Overall_Score
+      ],
+      "markers": []
+    }
+  ];
 
     const MyResponsiveBullet = ({ data }) => (
         <Box className={classes.root}>
         <ResponsiveBullet
             data={data}
             margin={{ top: 50, right: 90, bottom: 30, left: 90 }}
-            spacing={50}
+            spacing={30}
             titleAlign="start"
-            titleOffsetX={-70}
+            titleOffsetX={-50}
             measureSize={0.2}
             rangeColors="red_yellow_green"
             animate={true}
--- src/components/RunPanel/ScoreButton.js
+++ src/components/RunPanel/ScoreButton.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import { orange, blue } from '@material-ui/core/colors';
 import Tooltip from '@material-ui/core/Tooltip'
 import { PanelSwitchContext, OverallScoreContext } from "../PanelManager";
 
@@ -26,20 +24,17 @@ export default function ScoreButton (props){
   const panelHandler = React.useContext(PanelSwitchContext);
   const overallScore = React.useContext(OverallScoreContext);
 
-  const GDPRButtonMessage = "GDPR \n" + overallScore.GDPR;
-  const ControlButtonMessage = "Control \n" + overallScore.Control;
-
   return(
     <div>
       <Tooltip title="Click to view score breakdown" placement="top" enterDelay={500} leaveDelay={200}>
         <Button onClick={() => panelHandler('BreakdownPanel')} className={classes.button} >
           <Grid container spacing={3} direction="column">
             <Grid item className={classes.score}>
-                <Typography variant="caption"> GDPR </Typography>
+                <Typography variant="body2"> GDPR </Typography>
                 <Typography variant="h3"> {overallScore.GDPR} </Typography>
             </Grid>
             <Grid item className={classes.score}>
-              <Typography variant="caption"> Control </Typography>
+              <Typography variant="body2"> Control </Typography>
              <Typography variant="h3"> {overallScore.Control} </Typography>
             </Grid>
           </Grid>
